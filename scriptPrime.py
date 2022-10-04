67c4cebffa229f95f429eee8e474df731df13c6f
Artem Medvedev <artiebears@mail.ru>
2022-10-04 20:16:56 +0300

    added script

---
+++ scriptPrime.py
@@ -0,0 +1,17 @@
+from sys import argv
+
+
+def IsPrime(a):
+    isprime=True
+    for i in range(2,a):
+        if a%i==0:
+            isprime=False
+    return isprime
+
+a=int(argv[1])
+sum=0
+
+for b in range(1,a):
+    if IsPrime(b)==True:
+        sum=sum+b
+print(sum)
