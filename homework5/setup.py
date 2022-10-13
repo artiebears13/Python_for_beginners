f620334cb9c56c5e4d275bee26814ba778922f7f
Artem Medvedev <artiebears@mail.ru>
2022-10-13 09:32:00 +0300

    modified

--- homework5/setup.py
+++ homework5/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mypythonlib',
-    packages=find_packages(),
+    packages=find_packages(where="src"),
     version='0.1.0',
     description='My first Python library',
     author='Artem Medvedev',
